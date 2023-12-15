0005ce6beefc8eabc5c3ca5762d51c6edf938cee
lordware <82336845+lordware@users.noreply.github.com>
2023-12-15 10:05:49 +0100

    Update IEEE.cs

--- IEEE.cs
+++ IEEE.cs
@@ -1,83 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Globalization;
-using System.Linq;
+using System;
 
 namespace ConsoleApp3
 {
     class IEEE
     {
-        private string _enteredVal;
-        private float _actuallyVal;
-        private double _errorCon;
-        private string _hexVal;
-        private string _bin;
-
-        public string HexVal
-        {
-            get { return _hexVal; }
-        }
-
-        public string Bin
-        {
-            get { return _bin; }
-        }
-
-        public string EnteredVal
-        {
-            get { return _enteredVal; }
-            set { _enteredVal = value; }
-        }
-
-        public float ActuallyVal
-        {
-            get { return _actuallyVal; }
-        }
-
-        public double ErrorCon
-        {
-            get { return _errorCon; }
-        }
+        public string HexVal { get; private set; } = string.Empty;
+        public string Bin { get; private set; } = string.Empty;
+        public string EnteredVal { get; set; } = string.Empty;
+        public float ActuallyVal { get; private set; }
+        public double ErrorCon { get; private set; }
         
         private float toAct()
         {
-            return float.Parse(_enteredVal);
+            float.TryParse(EnteredVal, out float result);
+            return result;
         }
 
         private double calcError()
         {
-            var act = string.Format("{0:G17}", _actuallyVal);
-            var actNor = _actuallyVal.ToString();
+            var act = $"{ActuallyVal:G17}";
+            var actNor = ActuallyVal.ToString();
             actNor += new string('0', act.Length - actNor.Length);
-            double res = Math.Abs(Convert.ToDouble(act) - Convert.ToDouble(actNor));
-            return res;
+            return Math.Abs(Convert.ToDouble(act) - Convert.ToDouble(actNor));
         }
 
         private void Converts()
         {
-            var bytes = BitConverter.GetBytes(_actuallyVal);
+            var bytes = BitConverter.GetBytes(ActuallyVal);
             var i = BitConverter.ToInt32(bytes, 0);
-            this._hexVal = "0x" + i.ToString("X8");
-            this._bin = Convert.ToString(Convert.ToInt32(_hexVal, 16), 2).PadLeft(32, '0');
+            this.HexVal = "0x" + i.ToString("X8");
+            this.Bin = Convert.ToString(i, 2).PadLeft(32, '0');
         }
 
         public void validateInput()
         {
-            if (float.TryParse(this.EnteredVal, out float x))
-            {
-                return;
-            }
-            else
-            {
-                throw new ArgumentException($"Invalid input \"{this.EnteredVal}\"");
-            }
+            if (!float.TryParse(this.EnteredVal, out float _))
+                throw new ArgumentException($"Invalid input: \"{this.EnteredVal}\"");
         }
 
         public void Calculate()
         {
-            _actuallyVal = toAct();
-            _errorCon = calcError();
+            ActuallyVal = toAct();
+            ErrorCon = calcError();
             Converts();
         }
     }
