13c9ffa9dfa02076d7da0c9aa3b4fadf6a104ba1
lordware <82336845+lordware@users.noreply.github.com>
2023-12-15 09:42:39 +0100

    Add files via upload

---
+++ IEEE.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    class IEEE
+    {
+        private string _enteredVal;
+        private float _actuallyVal;
+        private double _errorCon;
+        private string _hexVal;
+        private string _bin;
+
+        public string HexVal
+        {
+            get { return _hexVal; }
+        }
+
+        public string Bin
+        {
+            get { return _bin; }
+        }
+
+        public string EnteredVal
+        {
+            get { return _enteredVal; }
+            set { _enteredVal = value; }
+        }
+
+        public float ActuallyVal
+        {
+            get { return _actuallyVal; }
+        }
+
+        public double ErrorCon
+        {
+            get { return _errorCon; }
+        }
+        
+        private float toAct()
+        {
+            return float.Parse(_enteredVal);
+        }
+
+        private double calcError()
+        {
+            var act = string.Format("{0:G17}", _actuallyVal);
+            var actNor = _actuallyVal.ToString();
+            actNor += new string('0', act.Length - actNor.Length);
+            double res = Math.Abs(Convert.ToDouble(act) - Convert.ToDouble(actNor));
+            return res;
+        }
+
+        private void Converts()
+        {
+            var bytes = BitConverter.GetBytes(_actuallyVal);
+            var i = BitConverter.ToInt32(bytes, 0);
+            this._hexVal = "0x" + i.ToString("X8");
+            this._bin = Convert.ToString(Convert.ToInt32(_hexVal, 16), 2).PadLeft(32, '0');
+        }
+
+        public void validateInput()
+        {
+            if (float.TryParse(this.EnteredVal, out float x))
+            {
+                return;
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid input \"{this.EnteredVal}\"");
+            }
+        }
+
+        public void Calculate()
+        {
+            _actuallyVal = toAct();
+            _errorCon = calcError();
+            Converts();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void clearConsole()
+        {
+            Console.Clear();
+            string x = " ";
+            Console.WriteLine("{0,40} Konverter für Fließkommazahlen", x);
+            Console.WriteLine("\n");
+        }
+                  
+        static void drawHud(IEEE x)
+        {
+            Console.WriteLine("                                            ----------------------------------------------");
+            Console.WriteLine("                                               Sign Exponent           Mantissa           ");
+            Console.WriteLine("Value:                                          {0}                                   ", x.Bin.Substring(0, 1).Replace("0", "+").Replace("1", "-"));
+            Console.WriteLine("Encoded as:                                     {0}     {1}              {2}            ", Convert.ToInt32(x.Bin.Substring(0, 1), 2), Convert.ToInt32(x.Bin.Substring(1, 8), 2), Convert.ToInt32(x.Bin.Substring(9, 23), 2));
+            Console.WriteLine("Binary:                                         {0}   {1}   {2}    ", x.Bin.Substring(0, 1), x.Bin.Substring(1, 8), x.Bin.Substring(9, 23));
+            Console.WriteLine("                                            ----------------------------------------------\n\n");
+        }
+        static void Menu()
+        {
+            IEEE iEEE = new IEEE();
+            clearConsole();
+
+            Console.WriteLine("You entered (Decimal): ");
+            iEEE.EnteredVal = Console.ReadLine();
+            iEEE.EnteredVal = iEEE.EnteredVal.Replace(".", ",");
+            clearConsole();
+            iEEE.validateInput();
+            iEEE.Calculate();
+            drawHud(iEEE);
+
+            Console.WriteLine("Decimal representation:                       {0}", iEEE.EnteredVal);
+            Console.WriteLine("Value actually stored in float:               {0:G17}", iEEE.ActuallyVal);
+            Console.WriteLine("Error due to conversion:                      {0}", iEEE.ErrorCon);
+            Console.WriteLine("Binary representation:                        {0}", iEEE.Bin);
+            Console.WriteLine("Hexadecimal Representation:                   {0}", iEEE.HexVal);
+
+            Console.ReadKey();
+            clearConsole();
+            return;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Menu();
+            }
+        }
+    }
+}
